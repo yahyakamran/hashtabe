343e3d0f8b76b492852ad7a980bbecf9871edd9b
yahyakamran <yahyakamran099@gmail.com>
2024-05-12 00:20:25 +0500

    Impleamented External Chaining Hashtable

--- hashtable.c
+++ hashtable.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <string.h>
+#include <strings.h>
+#include <time.h>
 
 #define TABLE_LEN 10
 #define NAME_MAX 256
 
-typedef struct{
+typedef struct student{
     char name[NAME_MAX];
     int roll_no;
+    struct student * next;
 } student;
 
 student * hashtable[TABLE_LEN];
@@ -24,11 +27,15 @@ void print_table(){
         if(hashtable[i] == NULL){
             printf("%d \t ------\n",i);
         }else{
-            printf("%d \t Name : %s   Roll Number : %d\n",
-                    i,
-                    hashtable[i]->name ,
-                    hashtable[i]->roll_no
-                    );
+            student * tmp = hashtable[i];
+            while (tmp != NULL) {
+                printf("%d \t Name : %s   Roll Number : %d\n",
+                        i,
+                        tmp->name ,
+                        tmp->roll_no
+                        );
+                tmp = tmp->next;
+            }
         }
     }
 }
@@ -36,67 +43,62 @@ void print_table(){
 int add_in_hashtable(student * p){
     if(!p) return 1;
     int index = hash(p->name);
-    if(hashtable[index] != NULL){
-        return 1;
-    }
+    p->next = hashtable[index];
     hashtable[index] = p;
     return 0;
 }
 
 student * find_in_hashtable(char * name){
     int index = hash(name);
-    if(hashtable[index] != NULL && strcmp(hashtable[index]->name,name) == 0){
-        return hashtable[index];
+    student * tmp = hashtable[index];
+    while(tmp != NULL &&
+            strncmp(tmp->name, name ,NAME_MAX) != 0){
+                tmp = tmp->next;
     }
-    else {
-        return NULL;
-    }
+    return  tmp;
 }
 
 student * delete_from_hashtable(char * name){
     int index = hash(name);
-    if(hashtable[index] != NULL && strcmp(hashtable[index]->name,name) == 0){
-        student * tmp = hashtable[index];
-        hashtable[index] = NULL;
-        return tmp;
+    student * temp = hashtable[index];
+    student * prev = NULL;
+    while (temp && strcmp(temp->name,name) != 0) {
+        prev = temp;
+        temp = temp->next;
     }
-    else {
-        return NULL;
+    if(temp == NULL) return NULL;
+    if(prev == NULL){
+        hashtable[index] = temp->next;
+    }else {
+        prev->next = temp->next;
     }
+    return temp;
 }
-
-void update_table(student * p){
-    int index = hash(p->name);
-    hashtable[index] = p;
-}
-
 int main (void){
     for(int i = 0 ; i < TABLE_LEN ; ++i){
         hashtable[i] = NULL;
     }
     student ali = {.name = "ali" , .roll_no = 10};
-    student ahmed = {.name = "ahmed" , .roll_no = 11};
     student yahya = {.name = "yahya" , .roll_no = 12};
+    student ahmed = {.name = "ahmed" , .roll_no = 11};
     student farah = {.name = "farah" , .roll_no = 13};
-    add_in_hashtable(&yahya);
     add_in_hashtable(&ali);
+    add_in_hashtable(&yahya);
     add_in_hashtable(&ahmed);
     add_in_hashtable(&farah);
     print_table();
-    student * tmp = find_in_hashtable("yahya");
+    student * tmp = find_in_hashtable("farah");
     if(tmp == NULL){
         printf("Not Found\n");
     }else{
         printf("Found %s\n",tmp->name);
     }
-    student * deleted = delete_from_hashtable("yahya");
+    student * deleted = delete_from_hashtable("ali");
     if(!deleted){
         printf("Not Found\n");
     }else{
         printf("Found and Deleted %s\n",deleted->name);
     }
-    print_table();
-    update_table(&ali);
     print_table();
     return 0;
 }
